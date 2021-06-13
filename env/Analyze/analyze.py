5bc52b7c57a93e00a12116d54489cd62c5a5741e
TaoMuRONG <13797500742@163.com>
2021-06-13 19:10:16 +0800

    数据分析部分完成

---
+++ env/Analyze/analyze.py
@@ -0,0 +1,59 @@
+from pandas import DataFrame
+from pandas import read_table
+from pandas import read_csv
+import pandas
+import numpy
+
+collectTxt = read_table('../Collect/collect.txt',sep="\t",header=0)
+collectCsv = read_csv('../Collect/collect.csv',sep=',',encoding='utf8',header=0,names=['单词','释义'])
+direction = read_csv('../Collect/direction.csv',sep=',',encoding='utf8',header=0,names=['地区','报考人数'])
+
+# 去除重复单词
+collectCsv = collectCsv.drop_duplicates('单词')
+
+# 去除重复地区
+direction = direction.drop_duplicates('地区')
+
+# 去除数据值为空的数据
+collectCsv = collectCsv.dropna()
+direction = direction.dropna()
+
+# 报考地区总数
+directionSum = direction.报考人数.count()
+
+# 报考人数平均值
+peopleAve = direction.报考人数.mean()
+
+# 报考人数最少地区
+peopleMin = direction.报考人数.min()
+
+# 报考人数最多地区
+peopleMax = direction.报考人数.max()
+
+# 报考总人数
+peopleSum = direction.报考人数.sum()
+
+# 分组分析
+# groupAnalyze = direction.groupby(by=['地区'])['报考人数'].agg({'报考地区总数': directionSum,'报考人数最少': peopleMin,'平均值':peopleAve,'报考人数最多':peopleMax,'报考总人数':peopleSum})
+
+groupAnalyze = direction.groupby(['地区','报考人数'])['报考人数'].agg(['count'])
+
+# 分布分析
+bins = [peopleMin-1,15,30,peopleMax+1]
+labels=['人数未到15万','15到30','人数超过30万']
+人数分层 = pandas.cut(direction.报考人数,bins,labels=labels,right=False)
+direction['人数分层'] = 人数分层
+group = direction.groupby(['人数分层'])['地区'].agg([numpy.size])
+
+# 数据抽取，将每个单词的首字母抽取出来
+initial = collectCsv['单词'].str.slice(0,1)
+
+# 单词总数
+totalWord = initial.sum()
+
+# 首字母为w的单词最多
+maxWord = initial.max()
+
+# 首字母为a的单词最少
+minWord = initial.min()
+
--- test.py
+++ test.py
@@ -1,25 +1,5 @@
-import csv
-import requests
-from bs4 import BeautifulSoup
+import numpy as np
 
-headers = {
-    'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'
-}
-#创建csv文件
-f = open('test.csv','wt',newline='')
-writer = csv.writer(f)
-#写入表头
-writer.writerow(('words','chs'))
-def get_words(url):
-    html = requests.get(url,headers=headers)
-    html.raise_for_status()
-    html.encoding = html.apparent_encoding
-    soup = BeautifulSoup(html.text,'lxml')
-    words = soup.select('body > div.wap12.clearfix.mtt > div.leftWidth.borT > div.artCon > div.artTxt > table:nth-child(3) > tbody > tr > td:nth-child(1)')
-    chs = soup.select('body > div.wap12.clearfix.mtt > div.leftWidth.borT > div.artCon > div.artTxt > table:nth-child(3) > tbody > tr > td:nth-child(2)')
-    for word,ch in zip(words,chs):
-        writer.writerow((word.get_text(),ch.get_text()))
-url='https://www.kuakao.com/english/ch/152726.html'
-get_words(url)
-f.close()
+M = np.array([[4,6,8,9],[8,5,4,2],[3,6,5,4]])
+for col in range(M.shape[1]):
+    print(M[:, col])
